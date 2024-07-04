6f0a17c0eefe70c0bcedd872fd2c96cc59b6a8a4
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-07-04 21:33:03 +0530

    Added firebase connection

---
+++ constants.ts
@@ -0,0 +1,7 @@
+export const APIKEY = process.env.APIKEY;
+export const AUTHDOMAIN = process.env.AUTHDOMAIN;
+export const PROJECTID = process.env.PROJECTID;
+export const STORAGEBUCKET = process.env.STORAGEBUCKET;
+export const MESSAGEINGSENDERID = process.env.MESSAGEINGSENDERID;
+export const APPID = process.env.APPID;
+export const MEASUREMENTID = process.env.MEASUREMENTID;

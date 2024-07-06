928515a6e513ed5e6a6932f5281bf6c3bf58dede
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-07-06 10:40:30 +0530

    Added basic styles for the frontend of the auth pages.

--- constants.ts
+++ constants.ts
@@ -1,7 +1,8 @@
-export const APIKEY = process.env.APIKEY;
-export const AUTHDOMAIN = process.env.AUTHDOMAIN;
-export const PROJECTID = process.env.PROJECTID;
-export const STORAGEBUCKET = process.env.STORAGEBUCKET;
-export const MESSAGEINGSENDERID = process.env.MESSAGEINGSENDERID;
-export const APPID = process.env.APPID;
-export const MEASUREMENTID = process.env.MEASUREMENTID;
+export const APIKEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string;
+export const AUTHDOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN as string;
+export const PROJECTID = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+export const STORAGEBUCKET = process.env.NEXT_PUBLIC_STORAGE_BUCKET as string;
+export const MESSAGEINGSENDERID = process.env
+  .NEXT_PUBLIC_MESSAGING_SENDER_ID as string;
+export const APPID = process.env.NEXT_PUBLIC_APP_ID as string;
+export const MEASUREMENTID = process.env.NEXT_PUBLIC_MEASUREMENT_ID as string;
